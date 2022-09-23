e791f78f67452ea7c7dd65c057df3179cdc6614f
Jonathan <jhyahav2@gmail.com>
2022-09-23 13:26:13 +0300

    test

--- db/mysql/migrations/02_add_event_data/migration.sql
+++ db/mysql/migrations/02_add_event_data/migration.sql
@@ -1,7 +1,3 @@
--- DropForeignKey
-ALTER TABLE `event` DROP FOREIGN KEY `event_ibfk_1`;
-ALTER TABLE `event` DROP FOREIGN KEY `event_ibfk_2`;
-
 DROP INDEX `event_created_at_idx` ON `event`;
 DROP INDEX `event_session_id_idx` ON `event`;
 DROP INDEX `event_website_id_idx` ON `event`;
@@ -29,13 +25,6 @@ CREATE TABLE `event`
     PRIMARY KEY (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- AddForeignKey
-ALTER TABLE `event` ADD CONSTRAINT `event_ibfk_2` FOREIGN KEY (`session_id`) REFERENCES `session`(`session_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
--- AddForeignKey
-ALTER TABLE `event` ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`website_id`) REFERENCES `website`(`website_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
-
-
 -- CreateTable
 CREATE TABLE `event_data` (
     `event_data_id` INTEGER NOT NULL AUTO_INCREMENT,
@@ -45,9 +34,6 @@ CREATE TABLE `event_data` (
     UNIQUE INDEX `event_data_event_id_key`(`event_id`),
     PRIMARY KEY (`event_data_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `event_data` ADD CONSTRAINT `event_data_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `event`(`event_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- CreateProcedureRenameIndex
 CREATE PROCEDURE `UmamiRenameIndexIfExists`(
